a8165e4bd2ca194372d38da6bbe5c79475749e61
MarlonBarbosa123 <134346574+MarlonBarbosa123@users.noreply.github.com>
2024-09-22 18:32:38 -0300

    Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,22 +4,22 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        
+        // colhendo numero.
         System.out.println("Informe um número para verificar se pertence à sequência de Fibonacci: ");
         int numero = scanner.nextInt();
         
-        
+        // base da sequencia
         int fib1 = 0;
         int fib2 = 1;
         
-       
+       // criando sequencia
         StringBuilder sequencia = new StringBuilder(fib1 + ", " + fib2);
         
-       
+       // verificando se o usuario digitou 0 ou 1 e informando que faz parte a sequencia
         if (numero == fib1 || numero == fib2) {
             System.out.println("O número " + numero + " pertence à sequência de Fibonacci.");
         } else {
-            
+            // verificando se o numero faz parte da sequencia
             int fibAtual = 0; 
             boolean pertence = false;
             
@@ -37,8 +37,8 @@ public class Fibonacci {
                 fib2 = fibAtual;
             }
             
-           
-           // System.out.println("Sequência de Fibonacci gerada: " + sequencia);
+           // mostrando sequencia (Opcional)
+           System.out.println("Sequência de Fibonacci gerada: " + sequencia);
             
             
             if (pertence) {
